4cb9fcfedbb12db56a301321c79f2cdc4de09985
Joginder Kumar <joginder.director@gmail.com>
2023-11-14 14:33:25 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -97,7 +97,6 @@ export const signIn = async (req, res) => {
     // Retrieve user details from the database
     const query = 'SELECT * FROM users WHERE email = ?';
     db.query(query, [email], async (err, results, fields) => {
-    db.end();
       if (err) {
         console.error('Error retrieving user:', err);
         return res.status(500).json({ error: 'Internal server error.' });
