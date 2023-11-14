4a0650a5e8d05287f2cafb6ac9817209c223497f
Joginder Kumar <joginder.director@gmail.com>
2023-11-14 14:30:41 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-import mysql from "mysql"
+import mysql from "mysql2"
 
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: '162.241.123.56', 
     user: 'bepojmes_rma',
     password: 'rma#@!123',
-    database: "bepojmes_rma"
-
+    database: "bepojmes_rma",
+    connectionLimit: 0
 });
 
 export default connection
