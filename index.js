d4fdef545de00d82cc15eb3a3dcd8abd710f1362
Joginder Kumar <joginder.director@gmail.com>
2023-11-14 12:20:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import connection from "./db.js";
 import express from "express";
 import bodyParser from "body-parser";
 
@@ -19,6 +20,28 @@ app.use(bodyParser.json());
 app.use(express.static('public/'));
 
 app.use('/images', express.static(__dirname + '/uploads'));;
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.log('Error when connecting to the database:', err);
+        setTimeout(handleDisconnect, 2000);
+    }
+    console.log("db is connected")
+});
+function handleDisconnect() {
+
+    connection.on('error', function(err) {
+      console.log('Database error:', err);
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        handleDisconnect();
+      } else {
+        throw err;
+      }
+    });
+  }
+  
+  handleDisconnect();
 
 
 app.use("/", testRoutes)
